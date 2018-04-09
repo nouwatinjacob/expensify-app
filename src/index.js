028ab12709ec228f08c786404e6dd919df71c6db
Nouwatin Jacob <jaysansa@gmail.com>
2018-04-09 23:27:38 +0100

    initial setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/style.scss';
+
+
+ReactDOM.render(<p>This my boilerplate</p>, document.getElementById("index"));
