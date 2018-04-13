e6d5f9d5ab66f24b78d6c28fc241bb0ab167ada1
Nouwatin Jacob <jaysansa@gmail.com>
2018-04-13 16:04:16 +0100

    wiring up add expense page functionality

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { addExpense } from '../actions/expenses';
+import ExpenseForm from './ExpenseForm';
 
-const AddExpensePage = () => (
+const AddExpensePage = (props) => (
   <div>
-    <h4>AddExpensePage</h4>
-    <p>
-      Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. 
-      Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-       Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. 
-       Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, 
-       imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. 
-    </p>
+    <h4>Add Expense</h4>
+    <ExpenseForm 
+      onSubmit={(expense) => {
+        props.dispatch(addExpense(expense));
+        props.history.push('/');
+      }}
+    />
   </div>
 );
 
-export default AddExpensePage;
+export default connect()(AddExpensePage);
