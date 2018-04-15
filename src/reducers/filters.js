8e59ab8426a0231683fa78153fd073d9d78a0af4
Nouwatin Jacob <jaysansa@gmail.com>
2018-04-15 22:40:00 +0100

    setup daterangepicker to filter expenses

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -1,10 +1,10 @@
-
+import moment from 'moment';
 // Filter Reducer
 const filtersReducerDefaultState = {
   text: '',
   sortBy: 'date',
-  startDate: undefined,
-  endDate: undefined
+  startDate: moment().startOf('month'),
+  endDate: moment().endOf('moment')
 };
 
 export default (state = filtersReducerDefaultState, action) => {
